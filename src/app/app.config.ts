d4cb6fd95c942a59dbcde433744912a690a58396
Chandramouli Veerubhotla <chandramouli.veerubhotla@gmail.com>
2024-12-07 14:24:52 +0530

    Configured ngx translate provided setting default language to english

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,31 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { HttpClient, provideHttpClient } from '@angular/common/http';
 import { provideRouter } from '@angular/router';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { provideTranslateService, TranslateLoader } from '@ngx-translate/core';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+
+// Factory function for translation loader
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
+
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideHttpClient(),
+    provideRouter(routes),
+    provideAnimationsAsync(), 
+    provideTranslateService({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient]
+      },
+      useDefaultLang: true,
+      defaultLanguage: 'en'
+    }),]
 };
